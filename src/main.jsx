a3ddb2c004f76aaeb31f4a3154964a1e6273e606
Sarzil727945 <sarzilmuntaha.gmail.com>
2023-05-17 17:14:25 +0600

    error page add

--- src/main.jsx
+++ src/main.jsx
@@ -14,12 +14,14 @@ import AuthProvider from './Provider/AuthProvider';
 import CheckOut from './components/CheckOut/CheckOut';
 import PrivateRoute from './PrivateRoute/PrivateRoute';
 import Bookings from './components/Bookings/Bookings';
+import ErrorPage from './components/ErrorPage';
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <Main></Main>,
+    errorElement:<ErrorPage></ErrorPage>,
     children: [
       {
         path: '/',
